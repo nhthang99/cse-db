8a490263eafb1d49e7aa475f691c5490a6ec27f2
Thang Nguyen <nhthang99@outlook.com>
2019-12-19 20:46:08 +0700

    Update

--- thecoffeehouse/news/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms  
-from news.models import NewsModel
-
-class NewsForm(forms.ModelForm):  
-    class Meta:  
-        model = NewsModel  
-        fields = "__all__" 
--- thecoffeehouse/news/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='news'),
-    path('show/', views.show, name="show"),
-    path('edit/<int:id>', views.edit),
-    path('delete/<int:id>', views.delete)
-]
